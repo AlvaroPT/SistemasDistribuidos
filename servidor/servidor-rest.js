bd2c08cb600e778ed825033141b9aa34c31a088f
AlvaroPastor <alvaro.pastor1997@gmail.com>
2022-09-19 14:30:03 +0200

    Añadido un index para centralizar las paginas

--- servidor/servidor-rest.js
+++ servidor/servidor-rest.js
@@ -15,6 +15,7 @@ var siguienteMuestra = datos.siguienteMuestra;
 //Accede al index html de nuestra carpeta indicada
 app.use("/medico", express.static("../medico"));
 app.use("/paciente", express.static("../paciente"));
+app.use("/index", express.static("../"));
 //app.use(express.static("../css" + '/public'));
 
 app.use(express.json()); // en el req.body tengamos el body JSON
@@ -26,75 +27,7 @@ function cambiarSeccion(seccion){
     document.getElementById(seccion).classList.add("activa");
     seccionActual=seccion;
 }
-
-//Comprueba si el login del medico es correcto y devuelve la id de ese medico.
-app.post("/api/medico/login", function(req,res){
-    var medico = {
-        login: req.body.login,
-        password: req.body.password
-    };
-    for(i in medicos){
-        if(medico.login == medicos[i].login && medico.password == medicos[i].password){
-            res.status(201).json(medicos[i].id);
-            console.log("id del medico logeado"+medicos[i].id)
-            return;
-        }   
-        
-    }
-    res.status(403).json("Médico o contraseña incorrecta");
-})
-
-//Elimina un paciente de la lista y sus muestras
-app.delete("/api/paciente/:id", function(req,res){
-    var check = true;
-    if (check == true){
-        var idPaciente =req.params.id;
-        var nuevasmuestras = [];
-        for(i in pacientes){
-            if(pacientes[i].id == idPaciente){
-                pacientes.splice(i, 1);
-            }
-        }
-        
-        for(i in muestras){
-            if(muestras[i].paciente != idPaciente){
-                nuevasmuestras.push(muestras[i]);
-            }
-        }
-        muestras = nuevasmuestras;
-        console.log(pacientes);
-        res.status(200).json("Paciente eliminado");
-        return;
-    }
-    
-    res.status(403).json("Paciente no encontrado.");
-})
-
-//Duplicar Muestras de un paciente en concreto
-app.post("/api/paciente/:id/duplicar", function(req,res){
-    var id = req.params.id;
-    
-    for(i in pacientes){
-        if(pacientes[i].id == id){
-            nuevopaciente = {
-                id : siguientePaciente,
-                nombre: pacientes[i].nombre,
-                fecha_nacimiento: pacientes[i].fecha_nacimiento,
-                genero: pacientes[i].genero,
-                medico: pacientes[i].medico,
-                codigo_acceso: pacientes[i].codigo_acceso,
-                observaciones: pacientes[i].observaciones
-
-            };
-            
-            pacientes.push(nuevopaciente);
-        }
-    }
-    siguientePaciente ++;
-    console.log(pacientes);
-    return res.status(201).json(pacientes);
-    
-})
+//GET
 //Compueba que el medico logeado existe en la lista.
 app.get("/api/medico/:id", function(req,res){
     var id = req.params.id;
@@ -163,6 +96,87 @@ app.get("/api/paciente/:id", function(req,res){
     }
     res.status(403).json("Médico no encontrado.");
 })
+//Devuelve un array con las variables
+app.get("/api/variable", function(req, res){
+    var listavariables = variables;
+    res.status(200).json(listavariables);
+})
+
+//Devuelve las muestras de un paciente.
+app.get("/api/paciente/:id/muestras", function(req,res){
+    var idpaciente = req.params.id;
+    listamuestras= [];
+    for(i in muestras){
+        if(muestras[i].paciente== idpaciente){
+            listamuestras.push(muestras[i]);
+        }
+    }
+    listamuestras.sort(function(a,b){
+        if(a.variable < b.variable){
+            return -1;
+        }
+        if(a.variable > b.variable){
+            return 1;
+        }
+        if(a.fecha < b.fecha){
+            return -1;
+        }
+        if(a.fecha > b.fecha){
+            return 1;
+        }
+        return 0;
+    });
+    res.status(200).json(listamuestras);
+    
+})
+
+
+//POST
+//Comprueba si el login del medico es correcto y devuelve la id de ese medico.
+app.post("/api/medico/login", function(req,res){
+    var medico = {
+        login: req.body.login,
+        password: req.body.password
+    };
+    for(i in medicos){
+        if(medico.login == medicos[i].login && medico.password == medicos[i].password){
+            res.status(201).json(medicos[i].id);
+            console.log("id del medico logeado"+medicos[i].id)
+            return;
+        }   
+        
+    }
+    res.status(403).json("Médico o contraseña incorrecta");
+})
+
+
+
+//Duplicar Muestras de un paciente en concreto
+app.post("/api/paciente/:id/duplicar", function(req,res){
+    var id = req.params.id;
+    
+    for(i in pacientes){
+        if(pacientes[i].id == id){
+            nuevopaciente = {
+                id : siguientePaciente,
+                nombre: pacientes[i].nombre,
+                fecha_nacimiento: pacientes[i].fecha_nacimiento,
+                genero: pacientes[i].genero,
+                medico: pacientes[i].medico,
+                codigo_acceso: pacientes[i].codigo_acceso,
+                observaciones: pacientes[i].observaciones
+
+            };
+            
+            pacientes.push(nuevopaciente);
+        }
+    }
+    siguientePaciente ++;
+    console.log(pacientes);
+    return res.status(201).json(pacientes);
+    
+})
+
 
 //Crea un nuevo paciente.
 app.post('/api/medico/:id/pacientes', function(req,res){
@@ -205,39 +219,31 @@ app.put('/api/paciente/:id', function(req,res){
     }
     
 })
-
-//Devuelve un array con las variables
-app.get("/api/variable", function(req, res){
-    var listavariables = variables;
-    res.status(200).json(listavariables);
-})
-
-//Devuelve las muestras de un paciente.
-app.get("/api/paciente/:id/muestras", function(req,res){
-    var idpaciente = req.params.id;
-    listamuestras= [];
-    for(i in muestras){
-        if(muestras[i].paciente== idpaciente){
-            listamuestras.push(muestras[i]);
+//Elimina un paciente de la lista y sus muestras
+app.delete("/api/paciente/:id", function(req,res){
+    var check = true;
+    if (check == true){
+        var idPaciente =req.params.id;
+        var nuevasmuestras = [];
+        for(i in pacientes){
+            if(pacientes[i].id == idPaciente){
+                pacientes.splice(i, 1);
+            }
         }
+        
+        for(i in muestras){
+            if(muestras[i].paciente != idPaciente){
+                nuevasmuestras.push(muestras[i]);
+            }
+        }
+        muestras = nuevasmuestras;
+        console.log(pacientes);
+        res.status(200).json("Paciente eliminado");
+        return;
     }
-    listamuestras.sort(function(a,b){
-        if(a.variable < b.variable){
-            return -1;
-        }
-        if(a.variable > b.variable){
-            return 1;
-        }
-        if(a.fecha < b.fecha){
-            return -1;
-        }
-        if(a.fecha > b.fecha){
-            return 1;
-        }
-        return 0;
-    });
-    res.status(200).json(listamuestras);
     
+    res.status(403).json("Paciente no encontrado.");
 })
+
 
 app.listen(8080);
